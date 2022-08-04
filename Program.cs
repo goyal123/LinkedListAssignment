bb1fb83a0750479555cbc1331f0502655d1d1ddc
goya123 <akhilg642@gmail.com>
2022-08-04 16:10:35 +0530

    calling AddFirst Method

--- Program.cs
+++ Program.cs
@@ -7,10 +7,19 @@
             Console.WriteLine("Wecome to Linked List");
 
             CustomLinkedList Mylist = new CustomLinkedList();
+            /*
             Mylist.AddLast(56);
             Mylist.AddLast(30);
             Mylist.AddLast(70);
             Mylist.display();
+            */
+
+            Mylist.AddFirst(70);
+            Mylist.AddFirst(30);
+            Mylist.AddFirst(56);
+            Mylist.display();
+
+
 
         }
     }
