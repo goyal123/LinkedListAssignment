d67c931f671c0c373bfee1087cbc064dc4badf82
goya123 <akhilg642@gmail.com>
2022-08-04 16:01:15 +0530

    Calling Addlast Faunction

--- Program.cs
+++ Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Wecome to Linked List");
+
+            CustomLinkedList Mylist = new CustomLinkedList();
+            Mylist.AddLast(56);
+            Mylist.AddLast(30);
+            Mylist.AddLast(70);
+            Mylist.display();
+
         }
     }
 }
