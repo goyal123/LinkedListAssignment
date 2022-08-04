c0a24160bd596debe8afd86ddacb2955970b25db
goya123 <akhilg642@gmail.com>
2022-08-04 16:00:31 +0530

    Implementing Linked List Function Addlast

---
+++ CustomLinkedList.cs
@@ -0,0 +1,38 @@
+﻿namespace LinkedList
+{
+    public class CustomLinkedList
+    {
+        public Node head;
+
+        public void AddLast(int data)
+        {
+            Node newnode = new Node(data);
+            if (head == null)
+                head = newnode;
+            else
+            {
+                Node temp = head;
+                while(temp.next!=null)
+                {
+                    temp = temp.next;
+                }
+                temp.next= newnode;
+            }
+
+        }
+
+        public void display()
+        {
+            Node temp = head;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+        }
+
+       
+        
+    }
+}
+
